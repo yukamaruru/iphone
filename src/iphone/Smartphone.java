74f46ddc6af6add2de2f4ddb98c65eeb795e3c0e
yukamaru <yuun.vv07@gmail.com>
2025-06-03 21:56:20 +0900

    オーバーライド

--- src/iphone/Smartphone.java
+++ src/iphone/Smartphone.java
@@ -2,6 +2,19 @@ package iphone;
 
 public class Smartphone extends Mp3player{
 	//extendsを使うことで、Mp3playerからSmartphoneへ継承するという意味。
+	public void play() {
+		System.out.println("再生(ハイレゾ機能)");
+	}
+	public void stop() {
+		System.out.println("停止(ハイレゾ機能)");
+	}
+	public void next() {
+		System.out.println("次へ(ハイレゾ機能)");
+	}
+	public void back() {
+		System.out.println("戻る(ハイレゾ機能)");
+	}
+	
 	public void call() {
 		System.out.println("電話");
 	}
